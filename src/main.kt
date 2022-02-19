291f31776fce2c4fa8e627b0987704cd2650599a
Respah-Nafula <nafularespah@gmail.com>
2022-02-18 22:35:58 -0500

    first commit

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+Name()
+    Modulus(81,7)
+    Sum(98,86,80,75)
+    interestingFactAboutMe()
+}
+
+fun Name(){
+    var name="Respah"
+    println("hello "+name)
+
+}
+fun Modulus(a: Int, b: Int){
+    var numbers=a%b
+    println(numbers)
+     }
+    fun Sum(a: Int,b: Int,  c: Int, d: Int){
+        var sum=a+b
+        println(sum)
+    }
+
+fun interestingFactAboutMe(){
+    var interestingPart="loves coding so much"
+    println("respah "+interestingPart)
+
+}
+
